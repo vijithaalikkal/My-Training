2a6d15f44fb6e2d51f7bb3015d0d539e2bde3cb1
vijithaalikkal <100674095+vijithaalikkal@users.noreply.github.com>
2022-03-01 13:34:25 +0530

    uploading my project

---
+++ SELF/Assitwo.java
@@ -0,0 +1,20 @@
+public class Assitwo
+{
+	
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		float c=4.5f;
+	
+		
+		int sum=a+b;
+		float sum2=c+a;
+		float Areaoftriangle=0.5f*a*b;
+		System.out.println("The sum of two integers is = " +sum);
+		System.out.println("The Area of Traingle is = "+Areaoftriangle);
+		System.out.println("The sum of Float & Integer = " +sum2);
+		
+	}
+	
+}
